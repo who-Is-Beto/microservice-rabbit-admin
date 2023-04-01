8ef06861356bec2402f49cf823f1287d35908657
whoIsBeto <whoisbeto@gmail.com>
2023-04-01 00:43:06 -0600

    [add] jwt verify and refresh tokens

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,9 @@
+import config from "config";
 import { DeepPartial } from "typeorm";
 import { User } from "../entities/user.entity";
+import redisClient from "../utils/connectRedis";
 import { AppDataSource } from "../utils/data-source";
+import { signJWT } from "../utils/jwt";
 
 const userRepository = AppDataSource.getRepository(User);
 
@@ -18,4 +21,20 @@ export const findUserById = async (userId: string) => {
 
 export const findUser = async (query: Object) => {
   return await userRepository.findOneBy(query);
+};
+
+export const signTokens = async (user: User) => {
+  redisClient.set(user.id, JSON.stringify(user), {
+    EX: config.get<number>("redisCacheExpiresIn") * 60
+  });
+
+  const accessToken = signJWT({ sub: user.id }, "accessTokenPrivateKey", {
+    expiresIn: `${config.get<number>("accessTokenExpiresIn")}m`
+  });
+
+  const refreshToken = signJWT({ sub: user.id }, "refreshTokenPrivateKey", {
+    expiresIn: `${config.get<number>("refreshTokenExpiresIn")}m`
+  });
+
+  return { accessToken, refreshToken };
 };
