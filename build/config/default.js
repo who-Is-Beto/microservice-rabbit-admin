4a7d8d5ed997592c1b81f930905bb9d81e2a1df3
whoIsBeto <whoisbeto@gmail.com>
2023-04-05 21:21:15 -0600

    [fix] app build file

--- build/config/default.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    origin: "http://localhost:3000",
-    accessTokenExpiresIn: 15,
-    refreshTokenExpiresIn: 60,
-    redisCacheExpiresIn: 60
-};
--- build/src/routes.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Routes = void 0;
-exports.Routes = [{}];
